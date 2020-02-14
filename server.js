079af8e7173f8a6a62e30d84ffd8636f4a873098
ZROSENSOHN LAPTOP <zac@satprepct.com>
2020-02-14 09:28:59 -0500

    updated package.json file and dynamic port in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 //initialize ID counter
 var counter = 0;
